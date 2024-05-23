aaab88d7a30474bc0ee1cfe4d1a2340a29988588
Alexander <dmolmar0201@g.educaand.es>
2024-05-23 20:30:07 +0200

    Añadido react

---
+++ proyecto-asir/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cabecera</h1>
+      </header>
+      <main className="App-content">
+        <h2>Contenido de Ejemplo</h2>
+        <p>Este es un ejemplo simple de una aplicación React.</p>
+      </main>
+      <footer className="App-footer">
+        <p>Pie de Página</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
